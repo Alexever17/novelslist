04561ed031bf94fe24f2bc64e18b27d97dc59b42
alexever17@gmail.com <alexever17@gmail.com>
2019-10-25 12:45:11 +0200

    trying out firebase

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <Nav/>
     <Explainer/>
-    <Carousel :novels=novels />
+    <Carousel :novels="novels"/>
+    <List/>
   </div>
 </template>
 
@@ -13,17 +14,36 @@ UIkit.use(Icons);
 import Nav from "./components/Nav.vue";
 import Explainer from "./components/Explainer.vue";
 import Carousel from "./components/Carousel.vue";
-import data from "./assets/data/data.json"
+import List from "./components/List.vue";
+import data from "./assets/data/data.json";
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+  measurementId: process.env.VUE_APP_measurementId
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export default {
   name: "app",
   components: {
-    Nav, Explainer, Carousel
+    Nav,
+    Explainer,
+    Carousel,
+    List
   },
   data() {
     return {
       novels: data
-    }
+    };
   }
 };
 </script>
@@ -34,6 +54,6 @@ export default {
 #app {
   background-image: url("assets/images/pipes.png");
   background-size: 6em 6em;
-  padding-bottom: 5em; 
+  padding-bottom: 5em;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="list">
+    <p>TEST</p>
+    <p>{{novels}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {},
+  methods: {},
+  created() {
+        this.$http.get(process.env.VUE_APP_databaseURL + 'posts.json').then(function(data){
+            return data.json()
+        }).then(function(data){
+            var array = [];
+            for (let key in data){
+                data[key].id = key;
+                array.push(data[key]);
+            }
+            this.novels = array;
+            //console.log(this.blogs);
+        });
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+#list {
+  margin: 0em 1em 1em 1em;
+}
+</style>
