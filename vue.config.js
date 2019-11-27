ffd632ecbeee6f5b459f29c22b9f08d85331134f
alexever17@gmail.com <alexever17@gmail.com>
2019-11-28 00:56:23 +0100

    extra pwa settings for correct pwa behaviour

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 //for development purposes
 
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== "production"
+  lintOnSave: process.env.NODE_ENV !== "production",
+  pwa: {
+    themeColor: '#39005A',
+    msTileColor: '#FFF'
+  }
 };
