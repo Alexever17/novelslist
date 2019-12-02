94b57f46e28afe51223987713fa8389a6e31ae7f
alexever17@gmail.com <alexever17@gmail.com>
2019-12-02 03:00:40 +0100

    fixing the bug in the preview of the table and commenting

--- src/components/NovelsList.vue
+++ src/components/NovelsList.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- table component container -->
+  <!-- table component container -->
   <div id="list">
     <!-- table title -->
     <div class="uk-card uk-card-default uk-card-body tabsBorder" id="listTitle">
@@ -40,10 +40,10 @@
         </li>
       </ul>
     </div>
-<!-- 2 version of the table are created because changing the component with css only was too complicated
+    <!-- 2 version of the table are created because changing the component with css only was too complicated
      one for desktop, one for mobile
-     this is the desktop one -->
-     <!-- takes in the options from the variable data in the assets -->
+    this is the desktop one-->
+    <!-- takes in the options from the variable data in the assets -->
     <vue-good-table
       id="table"
       :columns="columns"
@@ -52,8 +52,8 @@
       :pagination-options="tablePagination"
       :sort-options="tableSort"
     >
-    <!-- it has templates to transform the incoming entry data -->
-    <!-- some columns are hidden but are kept if they are needed later -->
+      <!-- it has templates to transform the incoming entry data -->
+      <!-- some columns are hidden but are kept if they are needed later -->
       <template slot="table-row" slot-scope="props">
         <!-- title visible -->
         <span v-if="props.column.field == 'Title'">
@@ -87,8 +87,7 @@
           <span
             :class="props.row.Dropped ? labelDanger : labelSuccess"
             v-if="props.row.Dropped"
-            >Dropped</span
-          >
+          >Dropped</span>
           <span class="uk-label tableLabel">{{ props.row.Origin }}</span>
         </span>
         <!-- origin, hidden -->
@@ -102,17 +101,16 @@
             :src="props.row.Picture"
             class="tablePicture"
             :alt="props.row.Title + ' Cover'"
-          />
+          >
         </span>
         <!-- opens up a modal with a button for further info, visible -->
         <span v-if="props.column.field == 'Link'">
-          <Modal :novel="props.row" uk-modal />
+          <Modal :novel="props.row" uk-modal/>
           <a
             class="uk-button uk-button-default tableInfo"
             type="button"
             :uk-toggle="'target: #' + 'modal' + modalIdMaker(props.row.Title)"
-            >More Info</a
-          >
+          >More Info</a>
         </span>
         <!-- ranking visible -->
         <span v-if="props.column.field == 'Rank'">
@@ -135,7 +133,7 @@
       :pagination-options="tablePaginationMobile"
       :sort-options="tableSortMobile"
     >
-    <!-- template to transfrom the incoming entry row -->
+      <!-- template to transfrom the incoming entry row -->
       <template slot="table-row" slot-scope="props">
         <!-- Information part with title, raniking and modalbutton and modal -->
         <span v-if="props.column.field == 'Title'">
@@ -145,7 +143,7 @@
               <h4 class="noMargin">Ranking: {{ props.row.Rank }} / 10</h4>
             </div>
             <div>
-              <Modal :novel="props.row" uk-modal />
+              <Modal :novel="props.row" uk-modal/>
               <!-- modalIdMaker makes the title to a functional ID -->
               <a
                 class="uk-button uk-button-default tableInfo"
@@ -153,8 +151,7 @@
                 :uk-toggle="
                   'target: #' + 'modal' + modalIdMaker(props.row.Title)
                 "
-                >More Info</a
-              >
+              >More Info</a>
             </div>
           </div>
         </span>
@@ -165,7 +162,7 @@
             :src="props.row.Picture"
             class="tablePictureMobile"
             :alt="props.row.Title + ' Cover'"
-          />
+          >
         </span>
       </template>
     </vue-good-table>
@@ -209,7 +206,8 @@ export default {
       tablePagination: variables.tablePagination,
       tablePaginationMobile: variables.tablePaginationMobile,
       columns: variables.columns,
-      columnsMobile: variables.columnsMobile
+      columnsMobile: variables.columnsMobile,
+      previewPositionCorrection: 15
     };
   },
   methods: {
@@ -217,47 +215,10 @@ export default {
     modalIdMaker(input) {
       return input.replace(/\W+/g, "");
     },
-    //generates a small preview part of the description on desktop
-    //it cuts after a sentence is over
-    //is not used when description is smaller the lengthChecker output number
-    preview(input) {
-      //gives back array [false or true, number where to cut the description]
-      let result = this.lengthChecker(input);
-
-      //lengthchecker returns an array with boolean if the description is longer than the cut for the width and 
-      //determined count
-      // 
-      if (result[0]) {
-        return input;
-      } else {
-        let operation = input.indexOf(". ", result[1] - 10);
-        if (operation == -1) {
-          return input;
-        } else {
-          return input.slice(0, input.indexOf(". ", result[1] - 10) + 2);
-        }
-        
-      }
-    },
-    // generates the overflow text after the preview
-    //is not used when description is smaller the lengthChecker output number
-    afterPreview(input) {
-      //gives back array [false or true, number where to cut]
-      let result = this.lengthChecker(input);
-
-      if (result[0]) {
-        return "---";
-      } else {
-        let operation = input.indexOf(". ", result[1] - 10);
-        if (operation == -1) {
-          return input;
-        } else {
-          return input.slice(input.indexOf(". ", result[1] - 10) + 2);
-        }
-      }
-    },
     //based on an exponential function return the number of characters that should be shown in the preview.
-    //returns an array with the boolean if description is longer than the determined char count and that count
+    //returns an array with the boolean if description is shorter than the determined char count 
+    //and returns that count
+    //also checks if the description is not cuttable at a sentence end and instead should be displayed in full
     lengthChecker(input) {
       let width = Math.max(
         document.documentElement.clientWidth,
@@ -265,12 +226,26 @@ export default {
       );
       let factor = Math.abs((-8 * width) / 1000 + 16);
       let division = Math.round(width / factor);
+      //determines the location of a sentence end at the location of the preview cut
+      let cutOfPoint = input.indexOf(". ", division - this.previewPositionCorrection);
 
-      if (input.length < division) {
-        return [true, 0];
+      if (input.length < division || cutOfPoint == -1) {
+        return [true, division];
       } else {
         return [false, division];
       }
+    },
+    //return the preview
+    preview(input) {
+      //cutting position
+      let lengthcheck = this.lengthChecker(input)[1] - this.previewPositionCorrection;
+      return input.slice(0, input.indexOf(". ", lengthcheck)+2);
+    },
+    //returns the rest of the description without the preview portion
+    afterPreview(input) {
+      //cutting position
+      let lengthcheck = this.lengthChecker(input)[1] - this.previewPositionCorrection;
+      return input.slice(input.indexOf(". ", lengthcheck)+2);
     },
     //filters the entries (all novels) based on the category selected
     tabsClick(input) {
